e1bf97cb4936dbcb58841f35d77441ca76111895
Robert Tkach <119821797+roberttkach@users.noreply.github.com>
2024-04-20 11:36:07 +0900

    Delete youtube-video-summarizer directory

--- youtube-video-summarizer/audio_processing.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-
-def convert_to_mono(audio_file):
-    sound = AudioSegment.from_file(audio_file)
-    sound = sound.set_channels(1)
-    mono_audio_file = "output.wav"
-    sound.export(mono_audio_file, format="wav")
-    return mono_audio_file
--- youtube-video-summarizer/models/vosk
+++
@@ -1,1 +0,0 @@
-https://alphacephei.com/vosk/models
